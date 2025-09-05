526cc7b469045d20966ad2e0e582e03bf3818e3f
yashraj8703 <rajyash8703@gmail.com>
2025-09-05 19:43:54 +0530

    added more features

--- Controller/ApplicationController.js
+++ Controller/ApplicationController.js
@@ -2,6 +2,7 @@ const Application = require("../Model/Application");
 const User = require("../Model/User");
 const Job = require("../Model/Job");
 
+//* authenticated users can apply to jobs
 exports.applyJob = async (req, res) => {
   try {
     const { jobId } = req.params;
@@ -32,15 +33,90 @@ exports.applyJob = async (req, res) => {
   }
 };
 
-exports.getApplicantsForJob = async (req, res) => {
+//get applied jobs by a user
+
+exports.getUserAppliedJobs = async (req, res) => {
   try {
-    const { jobId } = req.params;
+    const userId = req.user.userId;
+    const applications = await Job.find({ user: userId }).populate({
+      path: "job",
+      select:
+        "title company location employmentType requiredQualifications skillsRequired lastDateToApply",
+    });
+    res.status(200).json({
+      msg: "✅ Applied jobs fetched successfully",
+      total: applications.length,
+      applications,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error", error: err.message });
+  }
+};
 
+// listing applicantions - recruiters can see applicants who applied to thier job posts
+exports.getApplicantsForRecruiterJobs = async (req, res) => {
+  try {
+    const recruiterId = req.recruiter.recruiterId;
+    const jobsRecruiter = await Job.find({ recruiter: recruiterId });
+    if (!jobsRecruiter) {
+      return res.status(404).json({ message: "❌ No job posted yet" });
+    }
+    const jobId = jobsRecruiter.map((job) => job._id);
+
+    const applications = await Application.find({ job: { $in: jobId } })
+      .populate({
+        path: "user",
+        select: "username email resume", // applicant info
+      })
+      .populate({
+        path: "job",
+        select: "title company location", // job info
+      });
+    res.status(200).json({
+      msg: "✅ Applicants fetched successfully",
+      recruiterId,
+      totalJobs: jobsRecruiter.length,
+      totalApplications: applications.length,
+      applications,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error", error: err.message });
+  }
+};
+
+// applications for a specific job
+exports.getApplicantsForSpecificJob = async (req, res) => {
+  try {
+    const recruiterId = req.recruiter.recruiterId; // recruiter from token
+    const { jobId } = req.params; // specific job id from route
+
+    // check if job exists & belongs to this recruiter
+    const job = await Job.findOne({ _id: jobId, recruiter: recruiterId });
+    if (!job) {
+      return res
+        .status(404)
+        .json({ message: "❌ Job not found or not owned by this recruiter" });
+    }
+
+    // fetch applications for that job
     const applications = await Application.find({ job: jobId })
-      .populate("user", "_id username resume")
-      .populate("job", "title type location");
+      .populate({
+        path: "user",
+        select: "username email resume",
+      })
+      .populate({
+        path: "job",
+        select: "title company location",
+      });
 
-    res.status(200).json({ message: "Applicants fetched", applications });
+    res.status(200).json({
+      msg: "✅ Applicants fetched successfully",
+      jobId,
+      totalApplications: applications.length,
+      applications,
+    });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Server error", error: err.message });
--- Routes/Application.route.js
+++ Routes/Application.route.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const applicationController = require("../Controller/ApplicationController");
-const userAuth=require("../middleware/userAuth")
+const userAuth=require("../middleware/userAuth");
+const recruiterAuth = require("../middleware/recruiterAuth");
 
 
-router.post("/:jobId", userAuth,applicationController.applyJob);
-
-
-router.get("/", applicationController.getApplicantsForJob);
-
+router.post("/user-job-apply/:jobId", userAuth,applicationController.applyJob);
+router.get("/user-applied-jobs", userAuth,applicationController.getUserAppliedJobs);
+router.get("/recruiter-job-application",recruiterAuth, applicationController.getApplicantsForRecruiterJobs);
+router.get("/recruiter/:jobId",recruiterAuth,applicationController.getApplicantsForSpecificJob)
 module.exports = router;
